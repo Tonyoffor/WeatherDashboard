682734da8055351f01a22410cb65709ed105ba9c
Tonyoffor <tonyc.offor@gmail.com>
2022-11-18 09:27:48 -0500

    weather dashbord edits till this point

---
+++ Assets/index.js
@@ -0,0 +1,34 @@
+var APIKey = 'ae15a36edcdc75c39fec6b795ef5ca98';
+var searchbar = document.getElementById = 'searchbar';
+var city;
+var search_btn = document.getElementById = "search_btn"
+
+
+function getApi(){
+
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+
+    fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+        console.log(data)
+        for (var i = 0; i < data.length; i++)
+        var cityname = document.createElement('li');
+        var tableData = document.createElement('li');
+        var link = document.createElement('p');
+
+        // Setting the text of link and the href of the link
+        link.textContent = data[i].html_url;
+        link.href = data[i].html_url;
+
+        // Appending the link to the tabledata and then appending the tabledata to the tablerow
+        // The tablerow then gets appended to the tablebody
+        tableData.appendChild(link);
+        cityname.appendChild(tableData);
+        tableBody.appendChild(cityname);
+      }
+    )};
+
+
+    search_btn.addEventListener('click', getApi);
